07738b8720fd18dfbd80de89c09bcbea3893bffe
Минуллин Руслан Ильдусович <RIMinullin@dixy.local>
2023-11-14 10:44:44 +0300

    first commit

---
+++ excel_pandas.py
@@ -0,0 +1,111 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+def check_the_dir(dir_with_files):
+    """Проверить наличие папки, если нет, то создать."""
+    try:
+        os.mkdir(dir_with_files)
+    except FileExistsError:
+        print('dir exists')
+
+
+class MakerFiles:
+    """Создание xlsx-файлов в папке."""
+    def __init__(self, rows: int, cols: int):
+        self.rows = rows
+        self.cols = cols
+
+    def make_files_in_dir(self, number: int, start_number: int, dir_with_files: str):
+        """Создать заданное количество файлов xlsx в папке.
+        Принимает два числа: количество необходимых файлов и цифра,
+        с которой начинается нумерация файлов."""
+        start_number = start_number
+        for item in range(number):
+            df = pd.DataFrame(np.random.randint(
+                low=0, high=100, size=(self.rows, self.cols)),
+                )  # columns=list('COLS')
+            df.to_excel(f'{dir_with_files}/excel file number {start_number}.xlsx')
+            start_number += 1
+
+
+class CommonFile:
+    """Работа с общим файлом xlsx."""
+    def __init__(self, path_for_example: str):
+        self.example_for_common_file = path_for_example
+
+    def create_and_fill_common_file(self, path_to_files: str):
+        """Создать общий файл xlsx, куда подтянуть данные из всех файлов xlsx в папке."""
+        current_main_df = pd.read_excel(self.example_for_common_file)
+
+        # Перебираем файлы в папке, читаем добавляем в общий датафрейм.
+        for filename in os.listdir(path_to_files):
+            path_to_filename = path_to_files + '\\' + filename
+            if path_to_filename == self.example_for_common_file:
+                continue
+            df = pd.read_excel(path_to_files + '/' + filename)
+            buf_df = current_main_df._append(df, ignore_index=True)
+            current_main_df = buf_df
+
+        # Записываем общий датафрейм в Excel файл.
+        current_main_df.to_excel('main.xlsx', index=False)
+
+
+def check_the_files(path_to_files: str, path_to_common: str):
+    """Перебрать файлы в папке, пройтись по строкам в файлах и если
+    строки уже есть в общем файле, то пропустить, если нет, то добавить."""
+    current_main_df = pd.read_excel(path_to_common)
+    for filename in os.listdir(path_to_files):
+        df = pd.read_excel(path_to_files + '/' + filename)
+        for index, row in df.iterrows():
+            # Проверяем есть ли строка в общем файле, если нет, то добавляем.
+            if row.to_dict() not in current_main_df.to_dict(orient='records'):
+                current_main_df = current_main_df._append(row)
+    # Записываем общий датафрейм в Excel файл.
+    current_main_df.to_excel('main.xlsx', index=False)
+
+
+def validate_input(input_text):
+    """Проверка введенных пользователем значений. """
+    if not os.path.isdir(input_text):
+        raise ValueError("Указанной директории не существует")
+
+
+def create_files():
+    """ """
+    path_to_all_files = input("Введите полный путь до xlsx файлов, "
+                              "по которому будет создана соответствующся папка ")
+    validate_input(path_to_all_files)
+    number_of_rows = int(input("Введите число - количество строк в xlsx файлах: "))
+    number_of_cols = int(input("Введите число -  количество колонок в xlsx файлах: "))
+    number_of_files = int(input("Введите число - количество xlsx файлов: "))
+    start_number_for_file = int(input("Введите число - с которого начнется нумерация xlsx файлов: "))
+
+    check_the_dir(path_to_all_files)
+    maker_file = MakerFiles(number_of_rows, number_of_cols)
+    maker_file.make_files_in_dir(number_of_files, start_number_for_file, path_to_all_files)
+    return
+
+
+def create_common_file_and_check():
+    path_to_example_for_main = input("Введите полный путь до xlsx файла, "
+                                     "который будет взят за образец: ")
+    validate_input(path_to_example_for_main)
+    path_to_dir = input("Введите полный путь до директории с xlsx файлами, ")
+    validate_input(path_to_dir)
+
+    common_file = CommonFile(path_to_example_for_main)
+    common_file.create_and_fill_common_file(path_to_dir)
+
+
+def check_rows_in_files():
+    path_to_main = input("Введите полный путь до общего xlsx файла: ")
+    path_to_dir = input("Введите полный путь до директории с xlsx файлами: ")
+    check_the_files(path_to_dir, path_to_main)
+
+
+if __name__ == '__main__':
+    create_files()
+    create_common_file_and_check()
+    check_rows_in_files()
